6de259833447b1584b93ef09e913410249f279ac
Rohitsharma7877 <rohitsharma.sharma194@gmail.com>
2023-08-08 12:07:52 +0530

    addExcelsheet

--- integration/login.js
+++ integration/login.js
@@ -3,10 +3,10 @@ describe('Goodread', ()=>{
         cy.visit('https://www.goodreads.com/');
         cy.contains('Sign In').should('have.text', 'Sign In').click();
         cy.contains('Sign in with email').click();
-        cy.get('#ap_email').type('rohitsharma.sharma194@gmail.com');
-        cy.get('#ap_password').type('rohit123')
+        cy.get('#ap_email').type('rohit@gmail.com');
+        cy.get('#ap_password').type('rohit')
     })
 })
 
-
+//
 
